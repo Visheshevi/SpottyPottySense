9834f1c9b45d89c5397bc09ce49e2148002ca31a
Vishesh Chanana <visheshchanana1@gmail.com>
2023-07-23 18:43:39 -0400

    Update mqtt_listener.py

--- piServerService/mqtt_listener.py
+++ piServerService/mqtt_listener.py
@@ -3,15 +3,18 @@ import sys
 import os
 import logging
 
+sys.path.append('/home/serverthebest/Projects/Spotty Sense/SpottyPottySense/piServerService') 
+from spotify_start_on_device import spotify_token, spotify_device_id, setup_headers, request_device_to_play_spotify
+
 log_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),"mqtt_listener_servide_log.log")
 logging.basicConfig(filename=log_file, level=logging.DEBUG)
 
 #MQTT BROKER SETTINGS
 MQTT_BROKER_HOST = "localhost"
 MQTT_BROKER_PORT = 1883  # Default MQTT Port
-MQTT_TOPIC = "YOUR.TOPIC.NAME"
-MQTT_USERNAME = "MQTT_USERNAME"
-MQTT_PASSWORD = "MQTT_PASSWORD"
+MQTT_TOPIC = "YOUR_MQTT_TOPIC"
+MQTT_USERNAME = "USERNAME"
+MQTT_PASSWORD = "PASSWORD"
 
 
 # MQTT client setup
@@ -24,6 +27,8 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
 	payload = msg.payload.decode("utf-8")
 	logging.info(f"Received message: {payload}")
+	headers = setup_headers(spotify_token, spotify_device_id)
+	request_device_to_play_spotify(headers)
 	
 	
 # Setup Calback functions
@@ -45,3 +50,4 @@ except KeyboardInterrupt:
 		client.loop_stop()
 		client.disconnect()
 		logging.info("MQTT listener stopped")
+		
