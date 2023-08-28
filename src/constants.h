c1ff204e2c21f25b74bafac2428614bde61a4d2f
Vishesh Chanana <visheshchanana1@gmail.com>
2023-08-28 16:15:14 -0400

    Update constants.h

--- src/constants.h
+++ src/constants.h
@@ -5,7 +5,9 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-#define timeSeconds 2
+#define timeSeconds 1
+#define stopAfterMinutes 5
+#define motionDetectGap 2 // in minutes
  
 // Set GPIOs for LED and PIR Motion Sensor
 const int led = 14;
@@ -27,6 +29,8 @@ extern const char* mqtt_pass;
 extern const int mqtt_port;
 
 extern const char* motion_detect_topic;
+extern const char* spotify_running_question_topic;
+extern const char* spotify_running_answer_topic;
 
 extern WiFiClient espClient;
 extern PubSubClient client;
@@ -34,6 +38,7 @@ extern PubSubClient client;
 // MQTT function definitions
 void publish(const char* topic_name, const char* message);
 void setMQTTClient();
+bool mqttConnectedStatus();
 
 // WiFi function Defintions
 void connectToWifi();
